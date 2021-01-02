94ab8e46a87efeb4a41f5be7c766e24850ae00a1
Jameel Shehadeh <jamshehadeh@gmail.com>
2021-01-02 17:07:36 +0400

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Jameel Shehadeh on 02/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
